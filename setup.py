13de0ec1636e091ea43552dc8ceb0020c4fdf027
Tobias Werner <werner@mpib-berlin.mpg.de>
2024-09-07 11:23:30 +0200

    Bump version: 1.1.2 → 1.1.3

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ToFeWe/qpricesim',
-    version='1.1.2',
+    version='1.1.3',
     zip_safe=False,
 )
